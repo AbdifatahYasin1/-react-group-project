2dc5ab3bbe82fcaaf669300eaa0ee3b19b6e48f9
AbdifatahYasin1 <iskatag123@gmail.com>
2023-02-14 17:27:27 +0300

    Add router and used Navlink

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Missions from './components/Missions';
+import Rockets from './components/Rockets';
+import MyProfile from './components/Myprofile';
 
 function App() {
   return (
-    <Header />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/myprofile" element={<MyProfile />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,22 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 const Header = () => (
-  <div className="header">
+  <>
+    <nav className="navigation">
+      <div className="logo-container">
+        <img src={logo} className="logo" alt="logo" />
+        <h1>Space Travelers Hub</h1>
+      </div>
 
-    <div className="logo-container">
-      <img src={logo} className="logo" alt="logo" />
-      <h1>Space Travelers Hub</h1>
-    </div>
-    <div className="nav">
       <ul>
-        <li><a href="rockets">Rockets</a></li>
-        <li><a href="missions">Missions</a></li>
-        <li><a href="my profile">My Profile</a></li>
+        <li><NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Rockets</NavLink></li>
+        <li><NavLink to="/missions" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Missions</NavLink></li>
+        <li><NavLink to="/myprofile" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>My Profile</NavLink></li>
       </ul>
-
-    </div>
-  </div>
+    </nav>
+  </>
 
 );
 
